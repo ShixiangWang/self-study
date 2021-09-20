c37f2535e722f00d0ab08aff9592b1f15706443d
Shixiang Wang <w_shixiang@163.com>
2021-09-20 23:50:53 +0800

    update

---
+++ bioinformatics/biofx/06_hamm/solution1_abs_iterate.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Author : zhouliuyi <zhouliuyi@localhost>
+Date   : 2021-09-20
+Purpose: Rock the Casbah
+"""
+
+import argparse
+from typing import NamedTuple
+
+
+class Args(NamedTuple):
+    """ Command-line arguments """
+    seq1: str
+    seq2: str
+
+
+# --------------------------------------------------
+def get_args() -> Args:
+    """ Get command-line arguments """
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('seq1',
+                        metavar='str',
+                        help='Sequence 1')
+    parser.add_argument('seq2',
+                        metavar='str',
+                        help='Sequence 2')
+
+    args = parser.parse_args()
+
+    return Args(args.seq1, args.seq2)
+
+
+# --------------------------------------------------
+def main() -> None:
+    """ Make a jazz noise here """
+
+    args = get_args()
+    seq1, seq2 = args.seq1, args.seq2
+
+    l1, l2 = len(seq1), len(seq2)
+    distance = abs(l1 - l2)
+
+    for i in range(min(l1, l2)):
+        if seq1[i] != seq2[i]:
+            distance += 1
+
+    print(distance)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
